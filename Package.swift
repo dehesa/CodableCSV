d8b44132c605bd6decf7dbf920887e818053b619
Emil <emil@appunite.com>
2019-02-25 14:22:51 +0100

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "CodableCSV",
+    products: [
+        .library(
+            name: "CodableCSV",
+            targets: ["CodableCSV"])
+    ],
+    targets: [
+        .target(
+            name: "CodableCSV",
+            path: "Source")
+    ],
+    swiftLanguageVersions: [3, 4]
+)
