90ede7f961443721daf4c31a51d5505c074af4ed
Emil <emil@appunite.com>
2019-02-25 14:39:30 +0100

    add iso8601 formatter with support for older system

--- Souces/Utils/DateFormatter.swift
+++ Souces/Utils/DateFormatter.swift
@@ -1,10 +1,29 @@
 import Foundation
 
-internal extension DateFormatter {
-    /// `DateFormatter` for ISO 8610 date formats.
-    internal static let iso8601: ISO8601DateFormatter = {
-        let formatter = ISO8601DateFormatter()
-        formatter.formatOptions = .withInternetDateTime
-        return formatter
-    }()
+protocol DateFormatterProtocol {
+  func date(from string: String) -> Date?
+  func string(from date: Date) -> String
 }
+
+internal extension DateFormatter {
+  /// `DateFormatter` for ISO 8610 date formats.
+  internal static let iso8601: DateFormatterProtocol = {
+    if #available(OSX 10.12, *) {
+      let formatter = ISO8601DateFormatter()
+      formatter.formatOptions = .withInternetDateTime
+      return formatter
+    } else {
+      let formatter = DateFormatter()
+      formatter.locale = Locale(identifier: "en_US_POSIX")
+      formatter.timeZone = TimeZone(secondsFromGMT: 0)
+      formatter.calendar = Calendar(identifier: .iso8601)
+      formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+      return formatter
+    }
+  }()
+}
+
+extension DateFormatter: DateFormatterProtocol {}
+
+@available(OSX 10.12, *)
+extension ISO8601DateFormatter: DateFormatterProtocol {}
