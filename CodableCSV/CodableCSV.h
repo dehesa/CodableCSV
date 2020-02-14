24a96b7b0e3dce0fb65dcc3536491a25f979b529
Monica Rondón <rondonmonica@johndeere.com>
2020-02-14 11:53:27 +0100

    Removes xcode project from gitignore, re-adds xcode project

---
+++ CodableCSV/CodableCSV.h
@@ -0,0 +1,19 @@
+//
+//  CodableCSV.h
+//  CodableCSV
+//
+//  Created by Rondon Monica on 14.02.20.
+//  Copyright © 2020 Not Moni Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodableCSV.
+FOUNDATION_EXPORT double CodableCSVVersionNumber;
+
+//! Project version string for CodableCSV.
+FOUNDATION_EXPORT const unsigned char CodableCSVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableCSV/PublicHeader.h>
+
+
